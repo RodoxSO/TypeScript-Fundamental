68c945a69d9bc2a98488673d6a2a2023ef89a861
Rodolfo Onori <Rodolfo_Onori@hotmail.com>
2024-06-30 22:42:08 +0100

    Secao 2 - Variaveis e Tipagens

--- dist/index.js
+++
@@ -1,1 +0,0 @@
-(()=>{var n;(n=document.getElementById("content")).innerHTML="<h1>Ola mundo</h1>",n.innerHTML+="<h1>Ola mundo</h1>",n.innerHTML+="<h1>Ola mundo</h1>",n.innerHTML+="<h1>Ola mundo</h1>",n.innerHTML+="<h1>Ola mundo</h1>"})();
